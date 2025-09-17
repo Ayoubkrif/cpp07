318b112a29bc396fcaac9d82a76bbb52967868c6
Aykrifa <aykrifa@student.42.fr>
2025-09-17 13:53:43 +0200

    clarinette l abetarde

--- ex01/header/iter.hpp
+++ ex01/header/iter.hpp
@@ -6,15 +6,17 @@
 /*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 13:50:24 by aykrifa           #+#    #+#             */
-/*   Updated: 2025/09/16 14:19:04 by aykrifa          ###   ########.fr       */
+/*   Updated: 2025/09/17 09:51:55 by aykrifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
 
 template <typename T>
 
 void	iter(T *array, unsigned int size, void f(T const &))
 {
-	for (unsigned int i = 0; ++i; i < size)
+	for (unsigned int i = 0; i < size; ++i)
 	{
 		f(array[i]);
 	}
@@ -24,7 +26,7 @@ template <typename T>
 
 void	iter(T *array, unsigned int size, void f(T &))
 {
-	for (unsigned int i = 0; i < size; i++)
+	for (unsigned int i = 0; i < size; ++i)
 	{
 		f(array[i]);
 	}
